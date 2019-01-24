0534ee0b2c847e6093960cdac74b0c91075e7ad7
minimpoun <cdringenberg1@gmail.com>
2019-01-23 20:12:45 -0500

    fixed a problem when the files got merged

--- Engine/Source/Rendering/GraphicsContext.cpp
+++ Engine/Source/Rendering/GraphicsContext.cpp
@@ -5,11 +5,11 @@
 namespace MARS
 {
 
-<<<<<<< HEAD
 	GLFWwindow* FWindows_PlatformContext::InitContext()
 	{
 		return nullptr;
-=======
+	}
+
 	#pragma warning(push)
 	#pragma warning(disable:4100)
 	void FramebufferSize_Callback(GLFWwindow* InWindow, int32 InWidth, int32 InHeight)
@@ -25,29 +25,6 @@ namespace MARS
 			glfwPollEvents();
 			glfwSwapBuffers(InWindow);
 		}
-	}
-
-	/** End Callbacks */
-
-	void FMasterRenderContext::InitializeContexSytstem()
-	{
-		std::unique_ptr<FWindowContext> WindowContext(new FWindowContext());
-	}
-	
-	void FMasterRenderContext::InitializeRenderSystem()
-	{
-		Init();
-	}
-
-	void FMasterRenderContext::Init()
-	{
-		
-	}
-
-	FWindowContext::FWindowContext()
-	{
-		Init();
->>>>>>> 937241bc59ca51c55a7a4b49751867cccba3322a
 	}
 
 }
--- Engine/Source/Rendering/Renderer.cpp
+++ Engine/Source/Rendering/Renderer.cpp
@@ -2,22 +2,6 @@
 
 namespace MARS
 {
-<<<<<<< HEAD
-	void FRenderer::Init(GLADloadproc LoadProc)
-=======
-#pragma warning(push)
-#pragma warning(disable:4100)
-	void Renderer::Init(GLADloadproc LoadProc)
->>>>>>> 937241bc59ca51c55a7a4b49751867cccba3322a
-	{
-	
-	}
-
-	void FRenderer::SetRenderSize(uint32 NewWidth, uint32 NewHeight)
-	{
-
-	}
-#pragma warning(pop)
 
 	FORCEINLINE glm::vec2 FRenderer::GetRenderSize()
 	{
